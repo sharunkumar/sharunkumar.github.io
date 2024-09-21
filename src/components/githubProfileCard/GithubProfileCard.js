b2e2d936495b92b9cb3e1397664bcc4eb89e00a5
Sharun <sharun@sharunkumar.com>
2024-09-21 15:06:41 -0400

    test commit

--- src/components/githubProfileCard/GithubProfileCard.js
+++ src/components/githubProfileCard/GithubProfileCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import emoji from "react-easy-emoji";
 import {Fade} from "react-reveal";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
 import {contactInfo, isHireable} from "../../portfolio";
@@ -19,7 +20,7 @@ export default function GithubProfileCard({prof}) {
             <div className="blog-header">
               <p className="subTitle blog-subtitle">{contactInfo.subtitle}</p>
             </div>
-            {/* <h2 className="bio-text">"{emoji(String(prof.bio))}"</h2> */}
+            <h2 className="bio-text">"{emoji(String(prof.bio))}"</h2>
             {prof.location !== null && (
               <div className="location-div">
                 <span className="desc-prof">
