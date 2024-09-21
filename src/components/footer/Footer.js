3133cb2abca6c3e01bb60a1c7ce4fa3aa2b36c7c
Sharun <sharun@sharunkumar.com>
2024-09-21 15:39:53 -0400

    content: remove react-reveal references

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,15 +1,12 @@
 import React, {useContext} from "react";
-import {Fade} from "react-reveal";
 import StyleContext from "../../contexts/StyleContext";
 import "./Footer.scss";
 
 export default function Footer() {
   const {isDark} = useContext(StyleContext);
   return (
-    <Fade bottom duration={1000} distance="5px">
-      <div className="footer-div">
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}></p>
-      </div>
-    </Fade>
+    <div className="footer-div">
+      <p className={isDark ? "dark-mode footer-text" : "footer-text"}></p>
+    </div>
   );
 }
--- src/containers/skillProgress/skillProgress.js
+++ src/containers/skillProgress/skillProgress.js
@@ -1,44 +1,38 @@
 import React from "react";
-import "./Progress.scss";
-import {illustration, techStack} from "../../portfolio";
-import {Fade} from "react-reveal";
 import Build from "../../assets/lottie/build";
 import DisplayLottie from "../../components/displayLottie/DisplayLottie";
+import {illustration, techStack} from "../../portfolio";
+import "./Progress.scss";
 
 export default function StackProgress() {
   if (techStack.viewSkillBars) {
     return (
-      <Fade bottom duration={1000} distance="20px">
-        <div className="skills-container">
-          <div className="skills-bar">
-            <h1 className="skills-heading">Proficiency</h1>
-            {techStack.experience.map((exp, i) => {
-              const progressStyle = {
-                width: exp.progressPercentage
-              };
-              return (
-                <div key={i} className="skill">
-                  <p>{exp.Stack}</p>
-                  <div className="meter">
-                    <span style={progressStyle}></span>
-                  </div>
+      <div className="skills-container">
+        <div className="skills-bar">
+          <h1 className="skills-heading">Proficiency</h1>
+          {techStack.experience.map((exp, i) => {
+            const progressStyle = {
+              width: exp.progressPercentage
+            };
+            return (
+              <div key={i} className="skill">
+                <p>{exp.Stack}</p>
+                <div className="meter">
+                  <span style={progressStyle}></span>
                 </div>
-              );
-            })}
-          </div>
-
-          <div className="skills-image">
-            {illustration.animated ? (
-              <DisplayLottie animationData={Build} />
-            ) : (
-              <img
-                alt="Skills"
-                src={require("../../assets/images/skill.svg")}
-              />
-            )}
-          </div>
+              </div>
+            );
+          })}
         </div>
-      </Fade>
+
+        <div className="skills-image">
+          {illustration.animated ? (
+            <DisplayLottie animationData={Build} />
+          ) : (
+            <img alt="Skills" src={require("../../assets/images/skill.svg")} />
+          )}
+        </div>
+      </div>
     );
   }
   return null;
