307fd8983e456fe956e595b364a4843cb24a7fd4
Sharun <sharun@sharunkumar.com>
2024-10-22 02:08:10 -0400

    fix: gaps

--- src/components/ExperienceSubSection.tsx
+++ src/components/ExperienceSubSection.tsx
@@ -5,9 +5,9 @@ type Props = {
 
 export function ExperienceSubSection({ title, content }: Props) {
   return (
-    <div className="flex flex-col gap-1">
+    <div className="flex flex-col gap-0">
       <h4>{title}:</h4>
-      <ul className="flex list-disc flex-col gap-2 text-skin-muted [&>li]:ml-4 [&>li]:marker:text-skin-hue">
+      <ul className="flex list-disc flex-col gap-0 text-skin-muted [&>li]:ml-4 [&>li]:marker:text-skin-hue">
         {Array.isArray(content) ? (
           content.map((item, index) => <li key={index}>{item}</li>)
         ) : (
