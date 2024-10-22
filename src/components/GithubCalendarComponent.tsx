5e9d1d76f5a480ea4d771e2415c9e8183254ce84
Sharun <715417+sharunkumar@users.noreply.github.com>
2024-10-22 01:05:19 +0000

    feat: add github activity calendar (#7)
    - also adds a link next to each section

---
+++ src/components/GithubCalendarComponent.tsx
@@ -0,0 +1,22 @@
+import GitHubCalendar from "react-github-calendar";
+import nullthrows from "nullthrows";
+import { basics } from "@cv";
+
+const username = basics.profiles.find(
+  (profile) => profile.network === "GitHub",
+)?.username;
+
+export function GithubCalendarComponent() {
+  return (
+    <div className="text-skin-base">
+      <GitHubCalendar
+        username={nullthrows(username, "GitHub profile not found in cv.json")}
+        colorScheme="dark"
+        theme={{
+          dark: ["hsl(0, 0%, 22%)", "rgb(var(--color))"],
+        }}
+        hideMonthLabels
+      />
+    </div>
+  );
+}
