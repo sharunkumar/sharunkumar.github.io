3a1270a75bd80edc55c536c7d8c8698e202d2761
Sharun <sharun@sharunkumar.com>
2024-10-21 22:01:31 -0400

    implement og:image the right way

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import { readFileSync } from "fs";
+
+const CNAME = readFileSync("CNAME", "utf8");
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  site: `https://${CNAME}`,
 });
